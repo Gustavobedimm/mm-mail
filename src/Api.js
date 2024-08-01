981642bddd86c6c708795d7020ebfec27673a7ac
gustavobedimm <gustavo_bmazutti@hotmail.com>
2024-08-01 17:36:35 -0300

    baseUrl

--- src/Api.js
+++ src/Api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    //baseURL: 'https://mm-mail-back.vercel.app/'
-    baseURL: 'http://localhost:3000'
+    baseURL: 'https://mm-mail-back.vercel.app/'
+    //baseURL: 'http://localhost:3000'
 });
 
 export default api;
